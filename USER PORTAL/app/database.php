d7ee75734b984bbe2e6a02cacd1cafefed35bec1
Ankit Jain <56029974+Ankit-2312@users.noreply.github.com>
2020-07-15 21:55:51 +0530

    Update database.php

--- USER PORTAL/app/database.php
+++ USER PORTAL/app/database.php
@@ -2,22 +2,22 @@
 class Database{
  
     // specify your own database credentials
-    private $host_name = "sql100.epizy.com";
+    private $host = "sql100.epizy.com";
     private $db_name = "epiz_26185445_phplearning";
     private $username = "epiz_26185445";
-    private $password = "";
+    private $password = "8J8WqkzMUs";
     public $conn;
  
-    // get the data_base connection
-    public function_exists getConnection(){
+    // get the database connection
+    public function getConnection(){
  
         $this->conn = null;
  
         try{
-            $this->_>conn = new PDO("mysql:host=" . $this->host .. ";dbname=" . $this->db_name, $this->username, $this->password);
+            $this->conn = new PDO("mysql:host=" . $this->host . ";dbname=" . $this->db_name, $this->username, $this->password);
             $this->conn->exec("set names utf8");
         }catch(PDOException $exception){
-            echo "Connection error: " ..... $exception->getMessage();
+            echo "Connection error: " . $exception->getMessage();
         }
  
         return $this->conn;
