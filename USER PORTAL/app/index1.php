6d427cb0d79116b3c8350cb4e65e7520eee6b6e5
Ankit Jain <56029974+Ankit-2312@users.noreply.github.com>
2020-07-15 21:56:25 +0530

    Update index1.php

--- USER PORTAL/app/index1.php
+++ USER PORTAL/app/index1.php
@@ -24,10 +24,18 @@
           <label for="pass" class="label">Password</label>
           <input id="password" name="password" type="password" class="input" data-type="password" required>
         </div>
+        <div class="group">
+          <input id="check" type="checkbox" class="check" checked>
+          <label for="check"><span class="icon"></span> Keep me Signed in</label>
+        </div>
+        <div class="group">
           <input type="submit" class="button" value="Sign In">
         </div>
         <div class="hr"></div>
-		
+        <div class="foot-lnk">
+          <a href="#forgot">Forgot Password?</a>
+        </div>
+      </form>
       <form class="sign-up-htm" action="signup.php" method="POST">
         <div class="group">
           <label for="user" class="label">Username</label>
