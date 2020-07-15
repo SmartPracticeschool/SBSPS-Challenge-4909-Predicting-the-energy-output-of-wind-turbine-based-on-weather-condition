f13a6288ad78e629b9f14e0000f508e5432b31c1
Ankit Jain <56029974+Ankit-2312@users.noreply.github.com>
2020-07-15 22:31:03 +0530

    Update index1.php

--- USER PORTAL/app/index1.php
+++ USER PORTAL/app/index1.php
@@ -1,14 +1,14 @@
-<!DOCTYPE html>
+<!DOCTYPE HTML>
 <html lang="en" >
-<head>
-  <meta charset="UTF-8">
-  <title>ENERGY PREDICTION</title>
-  
-  <link rel = "icon" href = "https://www.freepngimg.com/download/orange/85889-thunder-text-triangle-lightning-png-image-high-quality.png" type = "image/x-icon">
-  <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
+  <head>
+      <meta charset="UTF-8">
+      <title>ENERGY PREDICTION</title>
+
+      <link rel = "icon" href = "https://www.freepngimg.com/download/orange/85889-thunder-text-triangle-lightning-png-image-high-quality.png" type = "image/x-icon">
+      <link rel='stylesheet prefetch' href='https://fonts.googleapis.com/css?family=Open+Sans:600'>
       <link rel="stylesheet" href="./assets/css/style.css">
   
-</head>
+
 <body>
   <div class="login-wrap">
   <div class="login-html">
@@ -17,13 +17,13 @@
     <div class="login-form">
       <form class="sign-in-htm" action="login.php" method="GET">
         <div class="group">
-          <label for="user" class="label">Username</label>
-          <input id="username" name="username" type="text" class="input" required>
-        </div>
+          <label class="label">Username</label>
+          <input id="username" name="user" type="text" class="input" required>
+
         <div class="group">
           <label for="pass" class="label">Password</label>
-          <input id="password" name="password" type="password" class="input" data-type="password" required>
-        </div>
+          <input id="password" name="pass" type="password" class="input" data-type="password" required>
+
         <div class="group">
           <input id="check" type="checkbox" class="check" checked>
           <label for="check"><span class="icon"></span> Keep me Signed in</label>
@@ -35,32 +35,25 @@
         <div class="foot-lnk">
           <a href="#forgot">Forgot Password?</a>
         </div>
-      </form>
+
       <form class="sign-up-htm" action="signup.php" method="POST">
-        <div class="group">
-          <label for="user" class="label">Username</label>
-          <input id="username" name="username" type="text" class="input" required>
-        </div>
-        <div class="group">
-          <label for="pass" class="label">Password</label>
-          <input id="password" name="password" type="password" class="input" data-type="password" required>
-        </div>
-        <div class="group">
-          <label for="pass" class="label">Confirm Password</label>
-          <input id="pass" type="password" class="input" data-type="password" required>
-        </div>
-        <div class="group">
-          <input type="submit" class="button" value="Sign Up">
-        </div>
-        <div class="hr"></div>
-        <div class="foot-lnk">
-          <label for="tab-1">Already Member?</a>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
-  
-  
-</body>
-</html>
+          <div class="group">
+            <label for="user" class="label">Username</label>
+            <input id="username" name="user" type="text" class="input" required>
+         
+          <div class="group">
+            <label for="pass" class="label">Password</label>
+            <input id="password" name="pass" type="password" class="input" data-type="password" required>
+         
+          <div class="group">
+            <label for="pass" class="label">Confirm Password</label>
+            <input id="pass" type="password" class="input" data-type="password" required>
+
+          <div class="group">
+            <input type="submit" class="button" value="Sign Up">
+
+          <div class="hr"></div>
+          <div class="foot-lnk">
+            <label for="tab-1">Already Member? </label>
+
+            
